5ab8c47bed9f2c68098caeb77eb8f7e20104baba
abidmehmod <160239077+abidmehmod@users.noreply.github.com>
2024-05-20 14:31:17 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// assigment of arrays
+//create an arrays named fruit that contain the following string element: "apple","banana","orange","mango"
+//question no 1 
+//  let fruits : string [] = ["apple","banana","mango","orange"];
+// declare an array named number that can contain only number element and initialize it with the value 10,20,30 and 40
+//question no 2
+//  let numbers : number [] = [10,20,30,40];
+//  console.log(numbers);
+//access the third element of fruits array and assign to a varible named third friut
+// quesion no 3 
+//  let fruits2 : string [] = ["apple","banana","mango","orange"];
+//  console.log(fruits2[2]); // mango
+//change the second element of the number arrys to 25
+//question no 4
+//  let numbers2 : number [] = [10,20,30,40];
+// numbers2[1] = 25
+// console.log(numbers2);
+//add the element "graph" to the end of the fruits arrays using the method
+// question no 5
+// let fruits2 : string [] = ["apple","banana","mango","orange"];
+// fruits2.push("graph");
+// console.log(fruits2);
+//remove the last element of the fruit arrays using the method and assign it to a varaible named lastfruit
+// question no 6
+// let lastFruit : string [] = ["apple","banana","mango","orange"];
+// lastFruit.pop();
+// console.log(lastFruit);
+//remove the first element from the fruits arrays using the method and assing it to a varaible named firstfruit
+//question no 7 
+// let firstFruit : string [] = ["apple","banana","mango","orange"];
+// firstFruit.shift();
+// console.log(firstFruit);
+//add the element "kiwi" to the begining of the friuts arrays using the method
+//question no 8
+// let myFruits : string [] = ["apple","banana","mango","orange"];
+// myFruits.unshift("kiwi");
+// console.log(myFruits);
+//remove 2 element from the friuts arrays starting from index using the method 
+//question no 9 
+// let removeFruit : string [] = ["apple","banana","mango","orange"];
+// removeFruit.shift();
+// removeFruit.shift();
+// console.log(removeFruit);
+// insert the element "pineapple" and "pears" at index 2 of the friut arrays using the method
+//question no 10
+// let firstFruit : string [] = ["apple","banana","mango","orange"];
+// firstFruit.unshift("pineapple","pears");
+// console.log(firstFruit);
+//create a new array named citrusfruit that contan the first 2 element of the fruit array using the method 
+//question no 11
+var citrusfruit = ["apple", "banana", "mango", "orange"];
+console.log(citrusfruit[0], citrusfruit[1]);
+//create a new array named lasttwofruit that contain the last two element of the fruit array using the method
+// question no 12
+var lasttwofruit = ["apple", "banana", "mango", "orange"];
+console.log(lasttwofruit[2], lasttwofruit[3]);
